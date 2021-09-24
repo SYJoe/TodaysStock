dae17a397bbcadf303a9af93db065b49c5a53f70
SYJoe <sungyeon52@gmail.com>
2021-09-24 11:23:40 +0000

    JSON Parsing Test #2 - Set JSON Encoding type

--- SIXTEENJEONJA/src/test.js
+++ SIXTEENJEONJA/src/test.js
@@ -1,14 +1,17 @@
 const axios = require("axios");
+const Iconv = require("iconv-lite");
 
 const options = {
   url: 'https://finance.naver.com/item/main.nhn?code=005930',
   method: 'GET',
-  headers: {'User-Agent':'Chrome/81.0.4044.92'}
+  headers: {'User-Agent':'Chrome/81.0.4044.92'},
+  responseType : "arraybuffer"
 };
 
 axios(options)
   .then(response => {
-    console.log(response.data);
+	const response_convert = Iconv.decode(response.data, "EUC-KR").toString();
+    console.log(response_convert);
   }).catch((error) => {
   console.error(error);
 });
